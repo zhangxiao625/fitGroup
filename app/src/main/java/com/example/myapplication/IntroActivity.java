28c9745470a187ca63a8aa8978e318cd5aadf1f5
zhangxiao625 <610962493@qq.com>
2021-11-10 08:52:32 -0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/IntroActivity.java
@@ -0,0 +1,137 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter;
+    TabLayout tabIndicator;
+    Button btnNext;
+    int position = 0;
+    Button btnGetStarted;
+    Animation btnAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // make the activity full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        // check if this is the first time app is opened
+        if (restorePrefData()) {
+            Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(mainActivity);
+            finish();
+        }
+        setContentView(R.layout.activity_intro);
+
+        btnNext = findViewById(R.id.btn_next);
+        btnGetStarted = findViewById(R.id.btn_get_started);
+        tabIndicator = findViewById(R.id.tab_indicator);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.btn_animation);
+
+        List<ScreenItem> mList = new ArrayList<>();
+        mList.add(new ScreenItem("Set Goals", "Use FitGroup to accomplish your goals. See your workout board, and select the goal you are going to complete today, and use calendar to track history or future workouts.",R.drawable.img1));
+        mList.add(new ScreenItem("Check in", "aa",R.drawable.background));
+        mList.add(new ScreenItem("Your Group", "aa",R.drawable.img1));
+        mList.add(new ScreenItem("Earn Points", "aa",R.drawable.img1));
+
+        //setup viewpager
+        screenPager = findViewById(R.id.screen_viewpager);
+        introViewPagerAdapter = new IntroViewPagerAdapter(this, mList);
+        screenPager.setAdapter(introViewPagerAdapter);
+
+        //setup tablayout with viewpager
+        tabIndicator.setupWithViewPager(screenPager);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                position = screenPager.getCurrentItem();
+                if (position < mList.size()) {
+                    position ++;
+                    screenPager.setCurrentItem(position);
+                }
+
+                if (position == mList.size() - 1) {
+                    loadLastScreen();
+                }
+            }
+        });
+
+        tabIndicator.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition() == mList.size() - 1) {
+                    loadLastScreen();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open main activity
+                Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivity);
+                
+                // save a boolean value to store
+                // the next time when the user runs the app, the intro activity won't show
+                savePrefsData();
+                finish();
+            }
+        });
+    }
+
+    private boolean restorePrefData() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs", MODE_PRIVATE);
+        Boolean isIntroActivityOpenedBefore = pref.getBoolean("isIntroOpened", false);
+        return isIntroActivityOpenedBefore;
+    }
+
+    private void savePrefsData() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("isIntroOpened", true);
+        editor.commit();
+    }
+
+    private void loadLastScreen() {
+        btnNext.setVisibility(View.INVISIBLE);
+        btnGetStarted.setVisibility(View.VISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
+        btnGetStarted.setAnimation(btnAnim);
+    }
+
+}
