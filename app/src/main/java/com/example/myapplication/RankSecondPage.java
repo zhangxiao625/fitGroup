d1362c01b045562ac5844e076fd0025ba871493b
Kshitij Sharma <ksharma7@illinois.edu>
2021-11-16 17:19:04 -0600

    added clickable calendar and piechart

--- app/src/main/java/com/example/myapplication/RankSecondPage.java
+++ app/src/main/java/com/example/myapplication/RankSecondPage.java
@@ -1,17 +1,65 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 
 public class RankSecondPage extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank_second_page);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.rank);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.feed:
+                        startActivity(new Intent(getApplicationContext(), FeedActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.rank:
+                        return true;
+                    case R.id.setting:
+                        startActivity(new Intent(getApplicationContext(), SettingActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+        CalendarView calendarView=(CalendarView) findViewById(R.id.calendarView1);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), "" + dayOfMonth, Toast.LENGTH_SHORT).show();// TODO Auto-generated method stub
+            }
+        });
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("EXTRA_name");
@@ -24,5 +72,20 @@ public class RankSecondPage extends AppCompatActivity {
         textView1.setText(name);
         textView2.setText("" + streakPoints);
 
-    }
+        //pie chart
+        PieChart pieChart;
+        pieChart = findViewById(R.id.piechart);
+
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Streak",
+                        streakPoints,
+                        Color.parseColor("#FFA726")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Workout",
+                        workoutPoints,
+                        Color.parseColor("#66BB6A")));
+
+    };
 }
