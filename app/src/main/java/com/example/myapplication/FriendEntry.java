3df170f6ae52284d8649c8ad412b39247ed93bee
Shikhar Solanki <shikhar4@illinois.edu>
2021-11-17 16:20:00 -0600

    added friend list to main page

---
+++ app/src/main/java/com/example/myapplication/FriendEntry.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+public class FriendEntry {
+
+    public int image_id;
+    public String name;
+    public int streak;
+
+
+    public FriendEntry(int image_id, String name, int streak) {
+        this.image_id = image_id;
+        this.name = name;
+        this.streak = streak;
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,20 +3,30 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-    ImageButton btn_play;
+    Button btn_play;
     Button btn_calendar, btn_choose_exercise;
 
     @Override
@@ -56,6 +66,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ArrayList<FriendEntry> arrayOfFriends = new ArrayList<FriendEntry>();
+        MainActivity.FriendListAdapter adapter = new MainActivity.FriendListAdapter(this, arrayOfFriends);
+        ListView listView = (ListView) findViewById(R.id.friend_list);
+        listView.setAdapter(adapter);
+
+        FriendEntry friend1 = new FriendEntry(R.drawable.easy_pose, "Daddy", 50);
+        FriendEntry friend2 = new FriendEntry(R.drawable.easy_pose, "Daddy", 50);
+        FriendEntry friend3 = new FriendEntry(R.drawable.easy_pose, "Daddy", 50);
+        FriendEntry friend4 = new FriendEntry(R.drawable.easy_pose, "Daddy", 50);
+        FriendEntry friend5 = new FriendEntry(R.drawable.easy_pose, "Daddy", 50);
+        FriendEntry friend6 = new FriendEntry(R.drawable.easy_pose, "Daddy", 50);
+        FriendEntry friend7 = new FriendEntry(R.drawable.easy_pose, "Daddy", 50);
+
+
+        adapter.add(friend1);
+        adapter.add(friend2);
+        adapter.add(friend3);
+        adapter.add(friend4);
+        adapter.add(friend5);
+        adapter.add(friend6);
+        adapter.add(friend7);
+
+
         btn_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,5 +113,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+    }
+
+    public class FriendListAdapter extends ArrayAdapter<FriendEntry> {
+
+        public FriendListAdapter(Context context, ArrayList<FriendEntry> FriendEntries) {
+
+            super(context, 0, FriendEntries);
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            // Get the data item for this position
+            FriendEntry friend = getItem(position);
+
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.layout_friend_entry, parent, false);
+            }
+
+            // Lookup view for data population
+            CircleImageView profileImage = (CircleImageView) convertView.findViewById(R.id.prof);
+            TextView friendName = (TextView) convertView.findViewById(R.id.tvFriendName);
+            TextView streak = (TextView) convertView.findViewById(R.id.tvStreak);
+
+
+            // Populate the data into the template view using the data object
+            profileImage.setImageResource(friend.image_id);
+            friendName.setText(friend.name);
+            streak.setText(Integer.toString(friend.streak) + " day streak");
+
+            // Return the completed view to render on screen
+            return convertView;
+        }
+
     }
 }
