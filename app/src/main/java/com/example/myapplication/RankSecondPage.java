3a8a330aec6773cc5ba067c104a4a89d6bf024c1
shikhar4 <shikharsolanki1@gmail.com>
2021-11-16 20:15:11 -0600

    created rank's third page, modified ui

--- app/src/main/java/com/example/myapplication/RankSecondPage.java
+++ app/src/main/java/com/example/myapplication/RankSecondPage.java
@@ -1,18 +1,53 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class RankSecondPage extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    Button thirdPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank_second_page);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.rank);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.feed:
+                        startActivity(new Intent(getApplicationContext(), FeedActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.rank:
+                        return true;
+                    case R.id.setting:
+                        startActivity(new Intent(getApplicationContext(), SettingActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
         Intent intent = getIntent();
         String name = intent.getStringExtra("EXTRA_name");
         int streakPoints = intent.getIntExtra("EXTRA_streakPoints", 0);
@@ -23,6 +58,16 @@ public class RankSecondPage extends AppCompatActivity {
 
         textView1.setText(name);
         textView2.setText("" + streakPoints);
+
+        // button to go to third page
+        thirdPage = findViewById(R.id.thirdPageButton);
+        thirdPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RankThirdPage.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
