be2d61fed9aa010dc26af198a1e94054613993b9
rsampat <rsampa4@illinois.edu>
2021-11-19 03:49:44 -0600

    Added search bar to find friends on platform. Autcomplete feature added as well.

--- app/src/main/java/com/example/myapplication/FeedActivity.java
+++ app/src/main/java/com/example/myapplication/FeedActivity.java
@@ -30,7 +30,14 @@ public class FeedActivity extends AppCompatActivity {
         Button btn6 = (Button) findViewById(R.id.button6);
         Button btn7 = (Button) findViewById(R.id.button7);
         Button btn8 = (Button) findViewById(R.id.button8);
+        Button btn9 = (Button) findViewById(R.id.button9);
+        Button btn10 = (Button) findViewById(R.id.button10);
+        Button btn11 = (Button) findViewById(R.id.button11);
+        Button btn12 = (Button) findViewById(R.id.button12);
+        Button btn13 = (Button) findViewById(R.id.button13);
+        Button btn14 = (Button) findViewById(R.id.button14);
 
+        // added to expand buttons
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,19 +116,6 @@ public class FeedActivity extends AppCompatActivity {
             }
         });
 
-        btn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog alertDialog = new AlertDialog.Builder(FeedActivity.this).create(); //Read Update
-                alertDialog.setTitle("Daniels's workout:");
-                alertDialog.setMessage("30 minute walk \n 5 minute sprint \n 45 minute walk \n 10 minute sprint");
-                alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {}
-                });
-                alertDialog.show();  //<-- See This!
-            }
-        });
-
         btn7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -140,6 +134,79 @@ public class FeedActivity extends AppCompatActivity {
             public void onClick(View v) {
                 AlertDialog alertDialog = new AlertDialog.Builder(FeedActivity.this).create(); //Read Update
                 alertDialog.setTitle("Will's workout:");
+                alertDialog.setMessage("70 minute run \n 100 push ups \n 80 minute swim");
+                alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {}
+                });
+                alertDialog.show();  //<-- See This!
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(FeedActivity.this).create(); //Read Update
+                alertDialog.setTitle("Tim's workout:");
+                alertDialog.setMessage("70 minute run \n 100 push ups \n 80 minute swim");
+                alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {}
+                });
+                alertDialog.show();  //<-- See This!
+            }
+        });
+        btn10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(FeedActivity.this).create(); //Read Update
+                alertDialog.setTitle("John's workout:");
+                alertDialog.setMessage("70 minute run \n 100 push ups \n 80 minute swim");
+                alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {}
+                });
+                alertDialog.show();  //<-- See This!
+            }
+        });
+        btn11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(FeedActivity.this).create(); //Read Update
+                alertDialog.setTitle("Rahul's workout:");
+                alertDialog.setMessage("70 minute run \n 100 push ups \n 80 minute swim");
+                alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {}
+                });
+                alertDialog.show();  //<-- See This!
+            }
+        });
+        btn12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(FeedActivity.this).create(); //Read Update
+                alertDialog.setTitle("Xiao's workout:");
+                alertDialog.setMessage("70 minute run \n 100 push ups \n 80 minute swim");
+                alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {}
+                });
+                alertDialog.show();  //<-- See This!
+            }
+        });
+        btn13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(FeedActivity.this).create(); //Read Update
+                alertDialog.setTitle("Myles' workout:");
+                alertDialog.setMessage("70 minute run \n 100 push ups \n 80 minute swim");
+                alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {}
+                });
+                alertDialog.show();  //<-- See This!
+            }
+        });
+        btn14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(FeedActivity.this).create(); //Read Update
+                alertDialog.setTitle("Nathan's workout:");
                 alertDialog.setMessage("70 minute run \n 100 push ups \n 80 minute swim");
                 alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {}
--- app/src/main/java/com/example/myapplication/SettingActivity.java
+++ app/src/main/java/com/example/myapplication/SettingActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,10 +21,20 @@ public class SettingActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
 
+    private static final String[] names = new String[]{"Shikhar Solanki",
+            "Max Wo", "Justin Su", "Greg Smith", "Daniel Brown", "Bob Joe", "Will Johnson",
+            "Tim Jones", "John Doe", "Rahul Sampat", "Kshitij Sharma", "Xiao Zhang"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        String[] names = getResources().getStringArray(R.array.array_names);
+        AutoCompleteTextView editText = findViewById(R.id.actv);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.custom_list_item, R.id.text_view_list_item, names);
+        editText.setAdapter(adapter);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.setting);
@@ -26,19 +42,18 @@ public class SettingActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
-                {
+                switch (item.getItemId()) {
                     case R.id.home:
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.feed:
                         startActivity(new Intent(getApplicationContext(), FeedActivity.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.rank:
                         startActivity(new Intent(getApplicationContext(), RankActivity.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.setting:
                         return true;
@@ -54,6 +69,7 @@ public class SettingActivity extends AppCompatActivity {
                     .replace(R.id.setting, new SettingActivity.SettingFragment())
                     .commit();
         }
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -66,5 +82,22 @@ public class SettingActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
+    }
+
+    public void onClick(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SettingActivity.this);
+        builder.setMessage("Add friend?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
